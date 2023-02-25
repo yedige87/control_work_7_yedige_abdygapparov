8835bb1073ba4851a9ffc026b49c0b1c2ae3ebc5
yedige87 <yedabdygapparov@gmail.com>
2023-02-25 17:20:25 +0600

    добавлен файл delete.html, добавлены стили, добавлены методы в app.py, расширен urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views.app import record_add_view, record_edit_view, record_delete_view
+from webapp.views.app import record_add_view, record_edit_view, record_delete_view, confirm_delete
 from webapp.views.base import index_view
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add', record_add_view, name='record_add'),
     path('edit/<int:pk>', record_edit_view, name='record_edit'),
     path('delete/<int:pk>', record_delete_view, name='record_delete'),
+    path('delete_yes/<int:pk>', confirm_delete, name='confirm_delete'),
 ]
--- webapp/views/app.py
+++ webapp/views/app.py
@@ -1,5 +1,7 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from webapp.models import GBook
 
 
 def record_add_view(request: WSGIRequest):
@@ -8,14 +10,23 @@ def record_add_view(request: WSGIRequest):
     }
     return render(request, 'index.html', context=context)
 
+
 def record_edit_view(request: WSGIRequest):
     context = {
         'key': 'value'
     }
     return render(request, 'index.html', context=context)
 
-def record_delete_view(request: WSGIRequest):
-    context = {
-        'key': 'value'
-    }
+
+def record_delete_view(request: WSGIRequest, pk):
+    record = get_object_or_404(GBook, pk=pk)
+    context = {'record': record}
+    return render(request, 'delete.html', context=context)
+
+
+def confirm_delete(request: WSGIRequest, pk):
+    record = get_object_or_404(GBook, pk=pk)
+    record.delete()
+    records = GBook.objects.all()
+    context = {'records': records}
     return render(request, 'index.html', context=context)
