ba4277dffee6d05738830a9a152caa929dc6b85a
yedige87 <yedabdygapparov@gmail.com>
2023-02-25 13:22:19 +0600

    Создана модель БД гостевой книги и адм. шаблон

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from webapp.models import GBook
+
+
+class GBookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'created_at')
+    list_filter = ('id', 'name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+    fields = ('text', 'name', 'email', 'created_at')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+
+admin.site.register(GBook, GBookAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class GBook(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False, verbose_name='Имя пользователя')
+    email = models.CharField(max_length=30, null=False, blank=False, verbose_name='EMail пользователя')
+    text = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Сообщение пользователя')
+    status = models.CharField(max_length=20, null=False, blank=False, verbose_name='Статус пользователя', default='active')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+
+    class Meta:
+        verbose_name = 'Пользователи'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['-created_at']
